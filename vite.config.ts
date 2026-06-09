4c1cdc14769ed2dd0b0279595854059c7622a122
quangnxse180723 <quangnxse180723@fpt.edu.vn>
2026-06-09 22:04:52 +0700

    chore: change COOP to unsafe-none

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+      'Cross-Origin-Opener-Policy': 'unsafe-none',
+      'Cross-Origin-Embedder-Policy': 'unsafe-none',
     },
   },
 })
