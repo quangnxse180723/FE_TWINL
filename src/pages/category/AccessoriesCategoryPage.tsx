119cfbe93aa5433211dfd606579e227a3213cbba
quangnxse180723 <quangnxse180723@fpt.edu.vn>
2026-08-08 14:13:03 +0700

    fix(ui): correct encoding issue in AccessoriesCategoryPage

--- src/pages/category/AccessoriesCategoryPage.tsx
+++ src/pages/category/AccessoriesCategoryPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+﻿import { useEffect, useState } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import productsApi, { type Product } from '../../api/products/productsApi'
 import { PATHS } from '../../routes/paths'
